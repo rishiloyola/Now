15a2b267bff0eb06336fdd6075c3a93e5c745c0f
Rishi <rishiloyola98245@gmail.com>
2015-09-21 22:58:34 +0530

    Update to Error handling

--- app/main.js
+++ app/main.js
@@ -1,49 +1,57 @@
-const HOSTNAME = "scalr.api.appbase.io"
-const APPNAME = "Check In"
-const USERNAME = "XXXX"
-const PASSWORD = "XXXX"
 //Initialize twitter and appbase modules
 var Twit = require('twit')
 var request = require("request");
 var appbase = require("appbase-js")
 var elasticsearch = require('elasticsearch')
+var fs = require("fs");
+var content = fs.readFileSync("config.json");
+var jsonContent = JSON.parse(content);
+const HOSTNAME = jsonContent.appbase.hostname
+const APPNAME = jsonContent.appbase.appname
+const USERNAME = jsonContent.appbase.username
+const PASSWORD = jsonContent.appbase.password
 //Required authetication of twitter api
 var T = new Twit({
-    	consumer_key: 'XXXX',        
-    	consumer_secret: 'XXXX',     
-    	access_token: 'XXXX',     
-    	access_token_secret: 'XXXX'
-	})
-
+      consumer_key: jsonContent.twitter.consumer_key,       
+      consumer_secret: jsonContent.twitter.consumer_secret,     
+      access_token: jsonContent.twitter.access_token,     
+      access_token_secret: jsonContent.twitter.access_token_secret
+  })
 var client = new elasticsearch.Client({
- 		host: 'https://'+USERNAME+":"+PASSWORD+"@"+HOSTNAME,
-	});
+    host: 'https://'+USERNAME+":"+PASSWORD+"@"+HOSTNAME,
+  });
 //Filter tweets related to swarmapp
 var stream = T.stream('statuses/filter', { track: 'swarmapp', language: 'en' })
 //Streaming of twitter tweets
 stream.on('tweet', function (tweet) {
-  var swarmappUrl = tweet.entities.urls[0].display_url;
-  var FSid = swarmappUrl.split("/")[2];
-  var FSurl = "https://api.foursquare.com/v2/checkins/resolve?shortId="+FSid+"&oauth_token=XXXX&v=20150919";
+  try{
+    var swarmappUrl = tweet.entities.urls[0].display_url;
+    var FSid = swarmappUrl.split("/")[2];
+    var FSurl = "https://api.foursquare.com/v2/checkins/resolve?shortId="+FSid+"&oauth_token="+jsonContent.foursquare.oauth_token+"&v=20150919";
   //Getting data from foursquare
-  request(FSurl, function(error, response, body) {
-  		var parsedbody = JSON.parse(body);
-  		var cityDetails = String(parsedbody.response.checkin.venue.location.city);
-  		if(!(cityDetails === "undefined")){
-  		  	console.log(cityDetails);
-  		  	//Storing data using appbase api
- 			client.index({
-  				index: 'Check In',
- 		  	  	type: cityDetails,
- 		  	  	id: parsedbody.response.checkin.id,
- 				body: body
-			}).then(function(response) {
-    			console.log(response);
-  		 	}, function(error) {
-   				console.log(error);
-  			});
-  		}         
+    request(FSurl, function(error, response, body) {
+       var parsedbody = JSON.parse(body);
+       if(parsedbody.meta.code==200){
+          var cityDetails = String(parsedbody.response.checkin.venue.location.city);
+          if(!(cityDetails === "undefined")){
+          //Storing data using appbase api
+            client.index({
+              index: 'Check In',
+              type: cityDetails,
+              id: parsedbody.response.checkin.id,
+              body: parsedbody.response.checkin
+            }).then(function(response) {
+              console.log(parsedbody.response);
+              console.log(response);
+            }, function(error) {
+              console.log(error);
+            });
+          }
+        }
     });
+  }catch(error){
+     console.log("an error occurred "+error);
+  } 
 })
 
 
