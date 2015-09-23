74207ea4608b27eb64a5d877002f2bf2c9ca52c2
rishiloyola <rishiloyola98245@gmail.com>
2015-09-24 02:03:32 +0530

    Changes in search engine

--- app/view/index.js
+++ app/view/index.js
@@ -5,6 +5,14 @@ myApp.service('client', function (esFactory) {
    });
 });
 myApp.controller('controller', function ($scope, client, esFactory) {
+
+    var streamingClient = new appbase({
+      url: 'https://scalr.api.appbase.io',
+      appname: 'Check In',
+      username: 'RhOzFtk2Y',
+      password: '9e9ddeb0-4e82-41bd-bc8d-39bb80f142bd'
+    });
+
     $scope.checktext = function(){
         try{
             if($scope.searchtext!=null && $scope.searchtext.replace(/\s/g,'').length){
@@ -21,7 +29,7 @@ myApp.controller('controller', function ($scope, client, esFactory) {
                 }, function (error, response) {
                     $scope.suggestions = response.mysuggester[0].options;
                     $scope.row = true;
-                    console.log(response.mysuggester[0].options[0].text);
+                    console.log(response);
                 });
             }else{
                 $scope.suggestions = null;
@@ -30,5 +38,27 @@ myApp.controller('controller', function ($scope, client, esFactory) {
         }catch(e){
             console.log('error');
         }
+    }
+    $scope.getData = function(){
+        streamingClient.streamSearch({
+            stream: true,     // we pass stream: true parameter to fetch current results and then stream new results.
+            type: 'books',
+            body: {
+               query : {
+                    filtered : { 
+                        query : {
+                            match: {
+                                city : $scope.searchtext
+                            } 
+                        }
+                    }
+                }
+            }
+        }).on('data', function(res) {
+    
+            console.log("city"+JSON.stringify(res.hits.hits[0]));
+        }).on('error', function(err) {
+            console.log("caught a stream error", err)
+        })
     }
 });
