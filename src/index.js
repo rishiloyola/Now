cc3f0f01703e4d0afd21b0ad5623b917931ee475
Rishi <rishiloyola98245@gmail.com>
2015-10-23 10:14:49 +0530

    Changes in suggestion method

--- src/index.js
+++ src/index.js
@@ -23,10 +23,12 @@ myApp.controller('viewcontroller',function ($scope, dataClient, esFactory, $inte
       $scope.checkinfrequency = "Count";
       $scope.$apply();
   };
+  
   $scope.opencheckin = function(event,details){
    $window.open('https://'+details,'_blank');
   };
-$scope.showwindow = function(e,data,visible){
+  
+  $scope.showwindow = function(e,data,visible){
    if(visible){
       infowindow.setContent('<table id="infowindow"><tr><td>' + '<img src="'+ data[6] +'">' + '</td>' + '<td>' + '<b>'+ data[8] + ' says ' +'</b>' + data[0] + '<br><b>Place : </b>' + data[7] + '</td></tr>'+'</table>');
       var center;
@@ -77,20 +79,21 @@ $scope.showwindow = function(e,data,visible){
       infowindow.setPosition(center);
       infowindow.setZIndex(2);
       infowindow.open($scope.mapobj);
-   }else{
+    }else{
       infowindow.close();
       infowindow = new google.maps.InfoWindow();
-   }
+    }
  };
+ 
   $scope.searchquerry = function(){
        try{
           //searchtext variable referred to the text in the search box
           if($scope.searchtext!=null && $scope.searchtext.replace(/\s/g,'').length){  //to check if search text is null
               var suggestClient = dataClient.getSuggestions($scope.searchtext);
-              suggestClient.then(function (resp) {
+              suggestClient.on('data',function (resp) {
                 $scope.row = true;
-                $scope.suggestions = resp.mysuggester[0].options;
-              }, function (err) {
+                $scope.suggestions = resp.suggest.mysuggester[0].options;
+              }).on('error',function (err) {
                   console.trace(err.message);
               });
           }else{
@@ -98,13 +101,15 @@ $scope.showwindow = function(e,data,visible){
               $scope.row = false;
           }
       }catch(e){
-          console.log('error');
+          console.log('error in try'+e);
       }
   };
+  
   $scope.changesearchtext = function(text){
        $scope.searchtext = text;
        $scope.row = false;
   }
+  
   $scope.getData = function(){
     var searchClient = dataClient.getSearchCheckin(angular.lowercase($scope.searchtext));
     searchClient.on('data', function(res) {
@@ -120,6 +125,7 @@ $scope.showwindow = function(e,data,visible){
         console.log("caught a stream error", err);
       });
   };
+  
   function searchProcess (response){
     if($scope.searchtext!=null && $scope.searchtext.replace(/\s/g,'').length){
       $scope.categoriesbox = true;
@@ -165,6 +171,7 @@ $scope.showwindow = function(e,data,visible){
        console.log($scope.places);
     }
  }
+ 
  var realtimeClient = dataClient.getliveData();
  realtimeClient.on('data', function(res) {
      streamProcess(res); //to fetch the data and to mark it on map
@@ -189,9 +196,6 @@ $scope.showwindow = function(e,data,visible){
      arr[11] = new Date().getTime()/1000;
      streamedCheckin.push(arr);
      renderarray.push(arr);
-    //  if(places!=null) renderarray.push.apply(renderarray,places);
-    //  renderarray.push.apply(renderarray,streamedCheckin);
-    //  if(draggedCheckin!=null) renderarray.push.apply(renderarray,draggedCheckin);
      $scope.places = renderarray;
      if(streamedCheckin.length<3){
        $scope.checkinfrequency=streamedCheckin.length;
@@ -207,56 +211,58 @@ $scope.showwindow = function(e,data,visible){
    }
  }
 
-     $scope.draggedmap = function(){
+   $scope.draggedmap = function(){
+     draggedCheckin = [];
+     if($scope.mapobj.getZoom()>=5){
+       var dragClient = dataClient.getDragData($scope.mapobj.getCenter().lat(),$scope.mapobj.getCenter().lng());
+       dragClient.on('data', function(res) {
+         dragProcess(res);  //to fetch the data and to mark it on map
+       }).on('error', function(err) {
+         console.log("caught a stream error", err);
+       });
+     }else if($scope.mapobj.getZoom()==4){
        draggedCheckin = [];
-       if($scope.mapobj.getZoom()>=5){
-         var dragClient = dataClient.getDragData($scope.mapobj.getCenter().lat(),$scope.mapobj.getCenter().lng());
-         dragClient.on('data', function(res) {
-           dragProcess(res);  //to fetch the data and to mark it on map
-         }).on('error', function(err) {
-           console.log("caught a stream error", err);
-         });
-       }else if($scope.mapobj.getZoom()==4){
-         draggedCheckin = [];
-         renderarray = [];
-         if(places!=null) renderarray.push.apply(renderarray,places);
-         if(streamedCheckin!=null) renderarray.push.apply(renderarray,streamedCheckin);
-         $scope.places = renderarray;
-       }
+       renderarray = [];
+       if(places!=null) renderarray.push.apply(renderarray,places);
+       if(streamedCheckin!=null) renderarray.push.apply(renderarray,streamedCheckin);
+       $scope.places = renderarray;
      }
-     function dragProcess(response){
-       if(response.hits){
-         for(var i=0;i<response.hits.hits.length;i++){
-           if( response.hits.hits[i]._source){
-             if(response.hits.hits[i]._source.latitude && response.hits.hits[i]._source.longitude){
-               if(citysearched==response.hits.hits[i]._source.city)
-               continue;
-               var arr = [];                 //creating array to publish details on map
-               arr[0] = response.hits.hits[i]._source.shout;
-               arr[1] = response.hits.hits[i]._source.latitude;
-               arr[2] = response.hits.hits[i]._source.longitude;
-               arr[3] = 1;
-               arr[4] = response.hits.hits[i]._source.category;
-               arr[5] = response.hits.hits[i]._source.url;
-               arr[6] = response.hits.hits[i]._source.photourl;
-               arr[7] = response.hits.hits[i]._source.venue;
-               arr[8] = response.hits.hits[i]._source.username;
-               arr[9] = 'orange_marker.png';
-               draggedCheckin.push(arr);
-             }
+   }
+   
+  function dragProcess(response){
+     if(response.hits){
+       for(var i=0;i<response.hits.hits.length;i++){
+         if( response.hits.hits[i]._source){
+           if(response.hits.hits[i]._source.latitude && response.hits.hits[i]._source.longitude){
+             if(citysearched==response.hits.hits[i]._source.city)
+             continue;
+             var arr = [];                 //creating array to publish details on map
+             arr[0] = response.hits.hits[i]._source.shout;
+             arr[1] = response.hits.hits[i]._source.latitude;
+             arr[2] = response.hits.hits[i]._source.longitude;
+             arr[3] = 1;
+             arr[4] = response.hits.hits[i]._source.category;
+             arr[5] = response.hits.hits[i]._source.url;
+             arr[6] = response.hits.hits[i]._source.photourl;
+             arr[7] = response.hits.hits[i]._source.venue;
+             arr[8] = response.hits.hits[i]._source.username;
+             arr[9] = 'orange_marker.png';
+             draggedCheckin.push(arr);
            }
          }
        }
-       if(draggedCheckin.length!=0){
-         renderarray = [];
-         if(places!=null) renderarray.push.apply(renderarray,places);
-         renderarray.push.apply(renderarray,draggedCheckin);
-         if(streamedCheckin!=null) renderarray.push.apply(renderarray,streamedCheckin);
-         $scope.places = renderarray;
-         $scope.$digest();
-         $scope.$apply();
-       }
      }
+     if(draggedCheckin.length!=0){
+       renderarray = [];
+       if(places!=null) renderarray.push.apply(renderarray,places);
+       renderarray.push.apply(renderarray,draggedCheckin);
+       if(streamedCheckin!=null) renderarray.push.apply(renderarray,streamedCheckin);
+       $scope.places = renderarray;
+       $scope.$digest();
+       $scope.$apply();
+     }
+   }
+   
  $scope.showcategory = function(data){
      places = [];
      if(categoryarray[data]==true) categoryarray[data]=false;
@@ -276,7 +282,7 @@ $scope.showwindow = function(e,data,visible){
      if(streamedCheckin!=null)renderarray.push.apply(renderarray,streamedCheckin);
      $scope.places = renderarray;
  };
- //
+ 
  function createJson(key,array){
     var json = [];
     for(var i=0;i<array.length;i++){
@@ -289,41 +295,39 @@ $scope.showwindow = function(e,data,visible){
     return json;
   }
 
-  var removecheckin = function (){
-    var nowTime = new Date().getTime()/1000;
-    if(streamedCheckin.length!=0){
-      if(citysearched==streamedCheckin[0][10]){
-        streamedCheckin[0][9] = 'red_marker.png';
-        searchedCheckinarray.push(streamedCheckin[0]);
-        categoryarray[streamedCheckin[0][4]] = true;
-        $scope.subjects = createJson(categoryarray,Object.keys(categoryarray));
-      }
-      for(var i=places.length+draggedCheckin.length,j=0;i<places.length+streamedCheckin.length+draggedCheckin.length;i++,j++){
-        if(nowTime-renderarray[i][11]<5)
+var removecheckin = function (){
+  var nowTime = new Date().getTime()/1000;
+  if(streamedCheckin.length!=0){
+    if(citysearched==streamedCheckin[0][10]){
+      streamedCheckin[0][9] = 'red_marker.png';
+      searchedCheckinarray.push(streamedCheckin[0]);
+      categoryarray[streamedCheckin[0][4]] = true;
+      $scope.subjects = createJson(categoryarray,Object.keys(categoryarray));
+    }
+    for(var i=places.length+draggedCheckin.length,j=0;i<places.length+streamedCheckin.length+draggedCheckin.length;i++,j++){
+      if(nowTime-renderarray[i][11]<5)
          break;
-        if(nowTime-renderarray[i][11]>=5){
+      if(nowTime-renderarray[i][11]>=5){
          renderarray.splice(i,1);
          streamedCheckin.splice(j,1);
-       }
       }
-      // if(places!=null)renderarray.push.apply(renderarray,places);
-      // if(draggedCheckin!=null) renderarray.push.apply(renderarray,draggedCheckin);
-      // if(streamedCheckin!=null)renderarray.push.apply(renderarray,streamedCheckin);
-         $scope.places = renderarray;
-      }
-      if(streamedCheckin.length<3){
+    }
+      $scope.places = renderarray;
+   }
+   if(streamedCheckin.length<3){
         $scope.checkinfrequency=streamedCheckin.length;
         $scope.color = 'rgb(255, 92, 92)';
-      }else if(streamedCheckin.length>=3 && streamedCheckin.length<10){
+   }else if(streamedCheckin.length>=3 && streamedCheckin.length<10){
         $scope.checkinfrequency=streamedCheckin.length;
         $scope.color = 'rgb(255, 153, 51)';
-      }else if(streamedCheckin.length>=10){
+   }else if(streamedCheckin.length>=10){
         $scope.checkinfrequency=streamedCheckin.length;
         $scope.color = 'rgb(51, 204, 51)';
-      }
+   }
       $scope.$digest();
       $scope.$apply();
   }
 
  $interval(removecheckin,5000);
+ 
 });
